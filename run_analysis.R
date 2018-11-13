493ec98dcbba2a858ef1cdc634eef69663f35096
tmbonner <42847275+tmbonner@users.noreply.github.com>
2018-11-13 16:10:11 -0500

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,8 +1,8 @@
- library(reshape2)
+ library(plyr)
  
  filename<- "getdata.zip"
 
-## Download and unzip the dataset
+## Download and unzip dataset
  if (!file.exists(filename)){
    fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip "
    download.file(fileURL, filename, method="curl")
